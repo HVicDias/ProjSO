b86e1ee326d62596796254b711abf74a2803fcd6
Henrique Dias <hvdias2015@gmail.com>
2021-09-13 09:09:41 -0300

    atualizacoes driver e keyboard

--- driverVideo.h
+++ driverVideo.h
@@ -92,12 +92,13 @@ void terminal_control(int control, const char* data, size_t size)
 {
 	char comando[20], codigo[20];
 	int comandoLen, codLen = 0;
+	size_t i;
 	if(control == 1){
 		terminal_write(data, size);
 		terminal_column = (size_t) 0;
 	}else if(control == 2){
 		terminal_row++;
-		for (size_t i = 0; i > size; i++){
+		for (i = 0; i > size; i++){
 			terminal_putchar('A');
 			terminal_row++;
 		}
