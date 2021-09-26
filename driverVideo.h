8c3838299aedc31d7389fde90deadfbb7f594978
Henrique Dias <hvdias2015@gmail.com>
2021-09-26 15:58:59 -0300

    correção

--- driverVideo.h
+++ driverVideo.h
@@ -6,6 +6,23 @@
 #include <unistd.h>
 #include <sys/reboot.h>
 
+/* keyboard interface IO port: data and control
+   READ:   status port
+   WRITE:  control register */
+#define KBRD_INTRFC 0x64
+ 
+/* keyboard interface bits */
+#define KBRD_BIT_KDATA 0 /* keyboard data is in buffer (output buffer is empty) (bit 0) */
+#define KBRD_BIT_UDATA 1 /* user data is in buffer (command buffer is empty) (bit 1) */
+ 
+#define KBRD_IO 0x60 /* keyboard IO port */
+#define KBRD_RESET 0xFE /* reset CPU command */
+ 
+#define bit(n) (1<<(n)) /* Set bit n to 1 */
+ 
+/* Check if bit n in flags is set */
+#define check_flag(flags, n) ((flags) & bit(n))
+
 int strcmp(const char *a, const char *b);
 
 static const size_t VGA_WIDTH = 80;
@@ -168,14 +185,25 @@ void info(){
 	terminal_row++;
 	terminal_column = (size_t) 0;
 }
-
+ 
 void reboot()
 {
-    uint8_t good = 0x02;
-    while (good & 0x02)
-        good = inb(0x64);
-    outb(0x64, 0xFE);
-    halt();
+    uint8_t temp;
+ 
+    asm volatile ("cli"); /* disable all interrupts */
+ 
+    /* Clear all keyboard buffers (output and command buffers) */
+    do
+    {
+        temp = inb(KBRD_INTRFC); /* empty user data */
+        if (check_flag(temp, KBRD_BIT_KDATA) != 0)
+            inb(KBRD_IO); /* empty keyboard data */
+    } while (check_flag(temp, KBRD_BIT_UDATA) != 0);
+ 
+    outb(KBRD_INTRFC, KBRD_RESET); /* pulse CPU reset line */
+loop:
+    asm volatile ("hlt"); /* if that didn't work, halt the CPU */
+    goto loop; /* if a NMI is received, halt again */
 }
 
 void controle(const char* data, size_t size)
